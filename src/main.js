1e80a0d8b8fad7034cc699ec99dbcf579db83bff
mortenhoelno <morten@mortenhoel.no>
2025-10-19 17:37:53 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.js";
+
+const rootEl = document.getElementById("root");
+createRoot(rootEl).render(React.createElement(App));
 
