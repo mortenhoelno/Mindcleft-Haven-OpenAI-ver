0690394d5c9be754d5e48f40bc557ede5d34a24e
mortenhoelno <morten@mortenhoel.no>
2025-10-19 17:37:29 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+import React from "react";
+import Game from "./components/Game.js";
+
+export default function App() {
+  // Ingen JSX for å slippe bygg-steg (ren ESM i nettleser)
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(Game, null)
+  );
+}
 
